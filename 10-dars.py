2cbae8f87048df14f646b96c225785c3457ec9cf
Temur Turdimurodov <98795209+Temurshoh@users.noreply.github.com>
2022-02-01 09:51:29 +0500

    yangi darslar qo'shildi
    9 , 10, 12-darslar qo'shildi

---
+++ 10-dars.py
@@ -0,0 +1,106 @@
+#cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+#for car in cars:
+    #if car!='gm':
+      #  print(car.title())
+   #else :
+      # print(car.upper())
+    
+    
+    
+#login=input("Iltimos ismingizni yozing:")
+#if login.lower()=="admin":
+    #print("Xush kelibsiz,Admin.Foydalanuvchilar ro'yhatini ko'rasizmi?")
+#else :
+   # print("Xush kelibsiz",login.title())
+        
+        
+        
+        
+# = input("Login kiriting: ")
+#if login.lower() == 'admin':
+   # print("Xush kelibsiz Admin, foydalanuvchilar ro'yxatini ko'rasizmi?")
+#else:
+    #print(f"Xush kelibsiz {login.title()}!")
+    
+    
+    
+    
+    
+    
+    
+#x=float(input("Birinchi soni kirting:"))
+#y=float(input("Ikkinci soni kiriting:"))
+#if x==y  :
+    #print("sonlar teng ekan x=y")
+    
+    
+    
+    
+#son=int(input("Istalgan son kiriting:"))
+#if son<0:
+    #print("Manfiy son")
+#else:
+    #print("Musbat son")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#son=int(input("Son kiriting:"))
+#if son>0 :
+   # print(son**(1/2)) 
+#else:
+   # print("Musbat son kiriting")
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
---
+++ 9-dars.py
@@ -0,0 +1,71 @@
+#ismlar=["temur","olim","sanjar","abu","asad"]
+#for ism in ismlar:
+   # print("Bugun uyga keling",ism)
+#print("Kod",len(ismlar),"marta takrorlandi")
+
+
+
+
+
+
+
+#sonlar=list(range(11,100,2))
+#for son in sonlar:
+   # print(son**3)
+   
+movies=[]
+#print("5 ta eng sevimli kinolaringiz")
+#for n in range(5):
+       #movies.append(input(f"{n+1}-kino:"))
+#print(movies)
+
+
+
+
+
+#kinolar = []
+#print("5 ta sevimli kinoingiz qaysilar?")
+#for k in range(5):
+   # kinolar.append(input(f"{k+1}-kino:"))
+#print(kinolar)  
+
+
+
+odamlar=int(input("Bugun nechi kishi bilan uchrashdingiz?>>>"))
+ismlar=[]
+for ism in range(odamlar):
+    ismlar.append(input(f"{ism+1}-uchrashgan odamingiz ismi:"))
+    print(ismlar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
